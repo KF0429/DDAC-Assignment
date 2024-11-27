2241bf27c56c7ebecf7ed469c16bf09ab1a83247
KF0429 <kamfei1987@gmail.com>
2024-11-27 17:47:56 +0800

    New components added

--- frontend/src/app/Components/SideBar.tsx
+++ frontend/src/app/Components/SideBar.tsx
@@ -25,6 +25,35 @@ export default function SideBar() {
         </svg>
         <h2 className="flex-1 font-bold text-base">Search Filter</h2>
       </div>
+      <fieldset className="border-0 border-b-[rgba(0,0,0,.09)] mt-5 mx-0 mb-0 px-0 pb-5 pt-0">
+        <legend className="mb-0 font-medium border-0 p-0 isolate block pl-2 pr-2">
+          Price Range
+        </legend>
+        <div className="block">
+          <div className="items-center flex justify-between mt-5 mb-[0.625rem] mx-0">
+            <input
+              className="bg-white border-[rgba(0,0,0,.26)] rounded-sm shadow-inset-sm border
+            text-[.8125rem] h-[1.875rem] outline-none pl-[.3125rem] w-[4.375rem] leading-normal m-0"
+              type="text"
+              placeholder="RM MIN"
+            />
+            <div className="bg-[#bdbdbd] flex-1 h-[1px] my-0 mx-[.625rem]"></div>
+            <input
+              className="bg-white border-[rgba(0,0,0,.26)] rounded-sm shadow-inset-sm border
+            text-[.8125rem] h-[1.875rem] outline-none pl-[.3125rem] w-[4.375rem] leading-normal m-0"
+              type="text"
+              placeholder="RM MAX"
+            />
+          </div>
+        </div>
+        <button
+          className="bg-[#ee4d2d] font-normal h-[1.875rem] mt-5 mb-0 mx-0 uppercase w-full outline overflow-visible relative
+        items-center border-0 rounded-sm shadow-ssm text-white cursor-pointer flex text-sm justify-center lette tracking-normal 
+        leading-[1] select-none"
+        >
+          Apply
+        </button>
+      </fieldset>
     </div>
   );
 }

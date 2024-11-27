7700e576936d9b038273b4dea7f80ff45364ea0f
KF0429 <kamfei1987@gmail.com>
2024-11-27 17:49:34 +0800

    fix some elements

--- frontend/src/app/lib/ProductMock.ts
+++ frontend/src/app/lib/ProductMock.ts
@@ -4,7 +4,7 @@ export const mockProducts = [
     imageUrl:
       "https://down-my.img.susercontent.com/file/my-11134211-7qula-li0a03rzu7l99e_tn.webp",
     title: "Apple AirPods with Charging Case (2019)",
-    price: "569.00",
+    price: 569.0,
     rating: 4.9,
     quantity: 15,
   },
@@ -14,7 +14,7 @@ export const mockProducts = [
       "https://down-my.img.susercontent.com/file/my-11134207-7rasb-m2as1u7xq6yle0_tn.webp",
     title:
       "Apple MacBook Air 13-inch M1 Chip (8 Core CPU/ 7 Core GPU / 256GB SSD)",
-    price: "3249.00",
+    price: 3249.0,
     rating: 4.0,
     quantity: 18,
   },
